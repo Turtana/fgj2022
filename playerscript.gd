403e43a4ce3a47a3c8c21604b3e938512aea87dd
Turtana <vahajylkkaelias@gmail.com>
2022-01-28 20:31:40 +0200

    fps

--- playerscript.gd
+++ playerscript.gd
@@ -4,7 +4,7 @@ extends KinematicBody
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var mouse_sens = .1
+var mouse_sensitivity = .001
 var camera_anglev = 0
 var move_speed = 5
 
@@ -13,12 +13,10 @@ var move_speed = 5
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var input = get_movement_input()
 	move_and_slide(input * move_speed)
-	
 
 func get_movement_input():
 	var input_dir = Vector3()
@@ -32,13 +30,13 @@ func get_movement_input():
 		input_dir += -$Camera.global_transform.basis.x
 	if Input.is_action_pressed("strafe_right"):
 		input_dir += $Camera.global_transform.basis.x
+	
+	input_dir.y = 0
 	input_dir = input_dir.normalized()
 	return input_dir
 
-func _input(event):
-	if event is InputEventMouseMotion:
-		$Camera.rotate_y(deg2rad(-event.relative.x*mouse_sens))
-		var changev=-event.relative.y*mouse_sens
-		print($Camera.rotation.x)
-		$Camera.rotate_x(deg2rad(changev))
-		$Camera.rotation_degrees.z = 0
+func _unhandled_input(event):
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		$Camera.rotate_x(-event.relative.y * mouse_sensitivity)
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		$Camera.rotation.x = clamp($Camera.rotation.x, -1.2, 1.2)
